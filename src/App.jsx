7765e7968ee6eea16ab63b097139c06fd81e9312
Sayeed <expsayeedshorif@gmail.com>
2025-06-26 20:47:13 +0600

    added tolist jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-      <h1>hello world</h1>  
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" fill='yellow' stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text"/>
+      </form>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import Demo from './Greeting.jsx'
 import Img from './Food.jsx'
+import { Todo } from './Todolist.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Todo/>
     <App />
     <Demo />
     <Img />
