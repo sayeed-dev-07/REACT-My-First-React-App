056571c3eb96893bb5962311b4e6f0bc38e88aeb
Sayeed <expsayeedshorif@gmail.com>
2025-06-26 08:05:27 +0600

    added some new imges jsx files too

---
+++ src/Food.jsx
@@ -0,0 +1,7 @@
+import './Food.css'
+function Img(){
+    return <div>
+        <img src="../src/assets/foodimg.jpg" alt="food-img" />
+    </div>
+}
+export default Img;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
+import App from './App.jsx'
 import Demo from './Greeting.jsx'
+import Img from './Food.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <App />
     <Demo />
+    <Img />
   </StrictMode>,
 )
