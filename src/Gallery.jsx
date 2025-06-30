32c5b8c1a3503be6f042b2579e0c9c4f9a10f196
sayeed <expsayeedshorif@gmail.com>
2025-06-30 13:22:14 +0600

    1st challenge done

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -6,68 +6,82 @@ function getImageUrl(imageId, size = 's') {
     '.jpg'
   );
 }
-function Profile({Scientist}){
-    return (
-        <section>
-        <h2>{}</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b> 
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (chemical element)
-          </li>
-        </ul>
-        </section>
-    )
+function Profile({ id, name, profession, awardsCount, awards, discovered, imgID }) {
+  return (
+    <section>
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imgID)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awardsCount} </b>
+          (
+          {awards.map((award, index) => (
+            <span key={award}>
+              {award}
+              {index + 1 !== awards.length ? ', ' : ''}
+            </span>
+          ))}
+          )
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-const Scientists = {
-     person1 : {
-        name : 'Maria Skłodowska-Curie',
-        profession : 'physicist and chemist',
-        awardsCount : 4,
-        awards : {
-            award1 : 'Nobel Prize in Physics',
-            award2 : 'Nobel Prize in Chemistry',
-            award3 : 'Davy Medal',
-            award4 : 'Matteucci Medal'
-        },
-        discoverd :'polonium (chemical element)',
-        imgID: 'szV5sdG'
-     },
-     person2 : {
-        name : 'Katsuko Saruhashi',
-        profession : 'geochemist',
-        awardsCount : 2,
-        awards : {
-            award1 : 'Miyake Prize for geochemistry',
-            award2 : 'Tanaka Prize'
-        },
-        discoverd :'a method for measuring carbon dioxide in seawater',
-        imgID: 'YfeOqp2'
-     }
-}
+const Scientists = [
+  {
+    id:1,
+    name: 'Maria Skłodowska-Curie',
+    profession: 'physicist and chemist',
+    awardsCount: 4,
+    awards: [
+      'Nobel Prize in Physics',
+      'Nobel Prize in Chemistry',
+      'Davy Medal',
+      'Matteucci Medal'
+    ],
+    discovered: 'polonium (chemical element)',
+    imgID: 'szV5sdG'
+  },
+  {
+    id:2,
+    name: 'Katsuko Saruhashi',
+    profession: 'geochemist',
+    awardsCount: 2,
+    awards: [
+      'Miyake Prize for geochemistry',
+      'Tanaka Prize'
+    ],
+    discovered: 'a method for measuring carbon dioxide in seawater',
+    imgID: 'YfeOqp2'
+  }
+];
+
 
 export default function Gallery() {
   return (
-    <div>
+    <>
       {Scientists.map(Scientist => {
-        
+        return (
+          <>
+          <Profile key={Scientist.id} {...Scientist} />
+          </>
+        )
       })}
-    </div>
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppButton from './Button.jsx'
+import Gallery from './Gallery.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppButton />
+    <Gallery />
   </StrictMode>,
 )
