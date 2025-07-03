f74277b08029a6a683b2d057d5588ea6995dd789
sayeed <expsayeedshorif@gmail.com>
2025-07-03 11:06:15 +0600

    added person component

---
+++ src/Person.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Person() {
+  const [person, setPerson] = useState({ name: "John", age: 100 });
+
+  const handleIncreaseAge = () => {
+    console.log("in handleIncreaseAge (before setPerson call): ", person);
+    setPerson((prevPerson)=>({ ...prevPerson, age: prevPerson.age + 1 }));
+    setPerson((prevPerson)=>({ ...prevPerson, age: prevPerson.age + 1 }));
+    // we've called setPerson, surely person has updated?
+    console.log("in handleIncreaseAge (after setPerson call): ", person);
+  };
+
+  // this console.log runs every time the component renders
+  // what do you think this will print?
+  console.log("during render: ", person);
+
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import FeedbackForm from './FeedBackForm'
 import Color from './Color'
+import Person from './Person'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Color />
+    <Person />
   </StrictMode>,
 )
