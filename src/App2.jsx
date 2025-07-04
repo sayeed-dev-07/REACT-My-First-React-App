84f7f2d75b5135f7c353dcea3badfafee01ca9c1
sayeed <expsayeedshorif@gmail.com>
2025-07-04 15:42:46 +0600

    added 2 more exercise

---
+++ src/App2.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+
+
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  // TODO: allow multiple selection
+  const selectedCount = selectedIds.length;
+
+  function handleToggle(toggledId) {
+    if(selectedIds.includes(toggledId)){
+      setSelectedIds(selectedIds.filter(id => id !== toggledId))
+    }else{
+      setSelectedIds([...selectedIds, toggledId])
+    }
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map(letter => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              // TODO: allow multiple selection
+              selectedIds.includes(letter.id)
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>
+            You selected {selectedCount} letters
+          </b>
+        </p>
+      </ul>
+    </>
+  );
+}
+const letters = [{
+  id: 0,
+  subject: 'Ready for adventure?',
+  isStarred: true,
+}, {
+  id: 1,
+  subject: 'Time to check in!',
+  isStarred: false,
+}, {
+  id: 2,
+  subject: 'Festival Begins in Just SEVEN Days!',
+  isStarred: false,
+}];
+function Letter({
+  letter,
+  onToggle,
+  isSelected,
+}) {
+  return (
+    <li className={
+      isSelected ? 'selected' : ''
+    }>
+      <label>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => {
+            onToggle(letter.id);
+          }}
+        />
+        {letter.subject}
+      </label>
+    </li>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import FeedbackForm from './FeedBackForm'
 import Color from './Color'
 import Person from './Person'
+import MailClient from './App2'
+import SyncedInputs from './App3'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Person />
+    <SyncedInputs />
   </StrictMode>,
 )
